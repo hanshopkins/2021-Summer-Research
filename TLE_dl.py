facd8d7b4273827df5a0b31af493e2773415fdb9
hanshopkins <69611816+hanshopkins@users.noreply.github.com>
2021-08-03 14:23:08 -0400

    Add files via upload

--- TLE_dl.py
+++ TLE_dl.py
@@ -4,9 +4,29 @@ import os
 
 while True:
 	infile = urllib.request.urlopen("https://www.celestrak.com/NORAD/elements/orbcomm.txt")
-	outfile = open("orbcomm_tles_"+repr(time.time())+".txt", "w")
+	outname = "orbcomm_tles_"+repr(time.time())+".txt"
+	outfile = open(outname, "w")
 	outfile.write(infile.read().decode('utf-8'))
 	infile.close()
 	outfile.close()
-	#os.system("scp -i ~/.ssh/<public key file> TLEs.txt username@niagara.scinet.utoronto.ca:/home/s/sievers/hans1/TLEs.txt") #this needs to be replaced with whatever key we use to access scinet
+	
+	try:
+		tempfile = open("latest.txt", "r")
+	except:
+		tempfile = open("latest.txt", "w")
+		tempfile.close()
+		tempfile = open("latest.txt", "r")
+	finally:
+		outfile = open(outname, "r")
+		if tempfile.read() == outfile.read():
+			outfile.close()
+			tempfile.close()
+			os.remove(outname)
+		else:
+			tempfile.close()
+			tempfile = open("latest.txt", "w")
+			outfile = open(outname, "r") #I don't know why this line is necessary buit it is
+			tempfile.write(outfile.read())
+			outfile.close()
+			tempfile.close()
 	time.sleep(3600)
