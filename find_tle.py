6cdf33bba331e63a85ba870c8f9e8c2aedbf3dfb
hanshopkins <69611816+hanshopkins@users.noreply.github.com>
2021-08-03 18:41:40 -0400

    Add files via upload

--- find_tle.py
+++ find_tle.py
@@ -2,9 +2,6 @@ import os
 
 def find_closest_file (ctime, directory = "/home/s/sievers/hans1/orbcomm_tles"):
 	tle_files = [f for f in os.listdir(directory) if (f[:13] == "orbcomm_tles_")]
-	#ctimes = []
-	#for f in tle_files:
-	#	ctimes.append(f[13:-4])
 	
 	if len(tle_files) == 0:
 		print("No tle files found in directory which start with 'orbcomm_tles_'")
