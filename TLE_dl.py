cdf2a95fffceee80e44d73de770bbeacea4032af
hanshopkins <69611816+hanshopkins@users.noreply.github.com>
2021-08-03 11:31:07 -0400

    Add files via upload

--- TLE_dl.py
+++ TLE_dl.py
@@ -4,9 +4,9 @@ import os
 
 while True:
 	infile = urllib.request.urlopen("https://www.celestrak.com/NORAD/elements/orbcomm.txt")
-	outfile = open("TLEs.txt", "w")
+	outfile = open("orbcomm_tles_"+repr(time.time())+".txt", "w")
 	outfile.write(infile.read().decode('utf-8'))
 	infile.close()
 	outfile.close()
-	os.system("scp -i ~/.ssh/<public key file> TLEs.txt username@niagara.scinet.utoronto.ca:/home/s/sievers/hans1/TLEs.txt") #this needs to be replaced with whatever key we use to access scinet
+	#os.system("scp -i ~/.ssh/<public key file> TLEs.txt username@niagara.scinet.utoronto.ca:/home/s/sievers/hans1/TLEs.txt") #this needs to be replaced with whatever key we use to access scinet
 	time.sleep(3600)
